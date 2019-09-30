5e253b2c8bc5edf46f460fa32671d61faa95499f
Bruce Hill <bhill@slac.stanford.edu>
2019-09-30 00:53:41 -0700

    Added support for NTTable PVs.
    Tested w/ all gateway NTTable rate PVs.

--- stressTest/pvGet.py
+++ stressTest/pvGet.py
@@ -8,6 +8,7 @@ import sys
 import textwrap
 import time
 import pdb
+import pprint
 
 #from functools import partial
 from p4p.client.thread import Context
@@ -25,6 +26,7 @@ class pvGetClient(object):
         self._pvName = pvName
         self._history = {}
         self._priorValue = None
+        self._noConnectionYet = True
         self._verbose = verbose
         self._checkPriorCount = checkPriorCount
         self._ctxt = Context( provider )
@@ -41,47 +43,69 @@ class pvGetClient(object):
     def callback( self, cbData ):
         pvName = self._pvName
         if isinstance( cbData, (RemoteError, Disconnected, Cancelled)):
+            if self._noConnectionYet and isinstance( cbData, Disconnected ):
+                return
             print( '%s: %s' % ( pvName, cbData ) )
             return
 
+        self._noConnectionYet = False
         pvValue = cbData
 
+        #pdb.set_trace()
         # Make sure we have a raw_stamp
+        raw_stamp = None
         if hasattr( pvValue, 'raw_stamp' ):
             raw_stamp = pvValue.raw_stamp 
-        else:
+        elif isinstance( pvValue, dict ):
+            if 'raw_stamp' in pvValue:
+                raw_stamp = pvValue[ 'raw_stamp' ]
+            if 'timestamp' in pvValue:
+                raw_stamp = pvValue[ 'timestamp' ] 
+            if 'timeStamp' in pvValue:
+                raw_stamp = pvValue[ 'timeStamp' ] 
+
+        if not raw_stamp or raw_stamp[0]:
             curTime = time.time()
-            raw_stamp = (	int(curTime) - EPICS2UNIX_EPOCH + EPICS2UNIX_EPOCH,
-                            (curTime - int(curTime)) * 1e9  )
+            raw_stamp = ( int(curTime), int((curTime - int(curTime)) * 1e9) )
 
         if isinstance( pvValue, p4p.nt.scalar.ntwrappercommon ):
-            if self._verbose:
-                print( '%s type: %s' % ( pvName, type(pvValue) ) )
+            return self.saveNtScalar( pvName, raw_stamp, pvValue )
 
-            if self._checkPriorCount:
-                newValue = int(pvValue)
-                if self._priorValue is not None:
-                    # Check for missed count
-                    expectedValue = self._priorValue + 1
-                    if expectedValue != newValue:
-                        print( '%s: missed %d counts!' % ( pvName, newValue - expectedValue ) )
-                self._priorValue	= newValue
-
-            # Save value
-            self.saveValue( pvName, raw_stamp, pvValue )
-            return
-
-        #pdb.set_trace()
         if isinstance( pvValue, p4p.wrapper.Value ):
+            if self._verbose:
+                print( '%s: ID=%s, type=%s' % ( pvName, pvValue.getID(), type(pvValue) ) )
+
             pvType = pvValue.type()
             if 'timeStamp' in pvValue:
-                raw_stamp = (	pvValue['timeStamp.secondsPastEpoch'],
-                                pvValue['timeStamp.nanoseconds'] )
-            #if pvValue.getID().startswith( 'epics:nt/NTTable:' ):
-            #	Need to loop through table rows
-            #	fullName = pvName + '.' + col[0].name + '.' + col[1].name
-            #	value = col[2].value
-            #else:
+                fieldTs = (	pvValue['timeStamp.secondsPastEpoch'],
+                            pvValue['timeStamp.nanoseconds'] )
+                if fieldTs[0]:
+                    raw_stamp = fieldTs
+
+            if pvValue.getID().startswith( 'epics:nt/NTTable:' ):
+                tableValue	= pvValue['value']
+                tableType	= pvType['value']
+                S, id, tableFields = tableType.aspy()
+                assert S == 'S'
+                assert id == 'structure'
+                tableItems = tableValue.items()
+                nCols = len(tableItems)
+                nRows = len(tableItems[0][1])
+                if self._verbose:
+                    print( "%s NTTable: nRows=%d, nCols=%d\n%s" % ( pvName, nRows, nCols, tableItems ) )
+                for row in range( nRows ):
+                    # Build up fullName
+                    fullName = pvName
+                    for col in range( nCols ):
+                        spec = tableFields[col][1]
+                        if spec == 'as':
+                            fullName += '.' + tableItems[col][1][row]
+                        elif spec != 'av' and spec != 'aU' and spec != 'aS':
+                            self.saveValue( fullName + '.' + tableFields[col][0], raw_stamp, tableItems[col][1][row] )
+                return
+
+            # This method works fpr p2p/Stats and potentially other
+            # simple PVStruct based PVs.
             #if pvValue.getID() == 'epics:p2p/Stats:1.0':
             for fieldName in pvValue.keys():
                 pvField = pvValue[fieldName]
@@ -90,10 +114,29 @@ class pvGetClient(object):
                     if fieldTs[0]:
                         raw_stamp = fieldTs
                 fullName = pvName + '.' + fieldName
-                if pvField.getID().startswith( 'epics:nt/NTScalar:' ):
-                    self.saveValue( pvName + '.' + fieldName, raw_stamp, pvField['value'] )
+                if isinstance( pvField, p4p.nt.scalar.ntwrappercommon ):
+                    return self.saveNtScalar( fullName, raw_stamp, pvField['value'] )
+                elif pvField.getID().startswith( 'epics:nt/NTScalar:' ):
+                    return self.saveValue( fullName, raw_stamp, pvField['value'] )
 
         # TODO: Handle other nt types
+
+    def saveNtScalar( self, pvName, raw_stamp, pvValue ):
+        if self._verbose:
+            print( '%s: ID=%s, type=%s' % ( pvName, pvValue.getID(), type(pvValue) ) )
+
+        if self._checkPriorCount:
+            newValue = int(pvValue)
+            if self._priorValue is not None:
+                # Check for missed count
+                expectedValue = self._priorValue + 1
+                if expectedValue != newValue:
+                    print( '%s: missed %d counts!' % ( pvName, newValue - expectedValue ) )
+            self._priorValue	= newValue
+
+        # Save value
+        self.saveValue( pvName, raw_stamp, pvValue )
+        return
 
     def saveValue( self, pvName, raw_stamp, value ):
         # assert pvValue.type() == Scalar:
@@ -144,13 +187,14 @@ def process_options(argv):
         argv = sys.argv[1:]
     description = 'pvGet.py is a python test program for monitoring PVs via PVAccess.\n'
     epilog_fmt =  '\nExamples:\n' \
-            + 'pvGet.py -p TEST:01:AnalogIn0\n'
+            + 'pvGet.py  TEST:01:AnalogIn0 TEST:02:Dig1\n'
     epilog = textwrap.dedent( epilog_fmt )
     parser = argparse.ArgumentParser( description=description, formatter_class=argparse.RawDescriptionHelpFormatter, epilog=epilog )
-    parser.add_argument( '-P', '--pvName',   dest='pvNames', action='append', \
+    parser.add_argument( 'pvNames', metavar='PV', nargs='+',
                         help='EPICS PVA pvNames Example: TEST:01:AnalogIn0', default=[] )
-    parser.add_argument( '-p', '--provider', action='store', default='pva', help='PV provider protocol, default is pva' )
-    parser.add_argument( '-f', '--input_file_path', action='store', help='Read list of pvNames from this file' )
+    parser.add_argument( '-m', '--monitor',  action='store', default=True, help='Stay connected and monitor updates.' )
+    parser.add_argument( '-p', '--provider', action='store', default='pva', help='PV provider protocol, default is pva.' )
+    parser.add_argument( '-f', '--input_file_path', action='store', help='Read list of pvNames from this file.' )
     parser.add_argument( '-v', '--verbose',  action="store_true", help='show more verbose output.' )
 
     options = parser.parse_args( )
@@ -160,11 +204,7 @@ def process_options(argv):
 def main(argv=None):
     options = process_options(argv)
 
-    if len(options.pvNames) == 0:
-        options.pvNames = [ 'PVA:GW:TEST:01:Count00', 'PVA:GW:TEST:02:Count00', 'PVA:GW:TEST:02:Count01' ]
     clients = []
-    #_ctxt	= Context('pva')
-    #pvValue = _ctxt.get( pvNames[0] )
     for pvName in options.pvNames:
         clients.append( pvGetClient( pvName, provider=options.provider, verbose=options.verbose ) )
 
@@ -177,7 +217,7 @@ def main(argv=None):
     for client in clients:
         client.closeSubscription()
     for client in clients:
-        client.writeValues('/tmp/fastClient')
+        client.writeValues('/tmp/TODO-add-dir-arg')
     time.sleep(1)
 
 if __name__ == '__main__':
