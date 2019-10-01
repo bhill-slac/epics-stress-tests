4f4bc2943e6e28623e3575dd69d7ea7d629e6611
Bruce Hill <bhill@slac.stanford.edu>
2019-09-30 19:53:29 -0700

    Add p4p to PYTHONPATH

--- stressTest/pcds_setup_env.sh
+++ stressTest/pcds_setup_env.sh
@@ -7,9 +7,16 @@ source $PSPKG_ROOT/etc/add_env_pkg.sh procServ/2.7.0-1.3.0
 
 echo $(echo EPICS_BASE=$EPICS_BASE)
 
-#P4P_TOP=/reg/neh/home/bhill/git-wa-neh/extensions/p4p-git 
-#PY_LD_VER=`python $P4P_TOP/get_PY_LD_VER.py`
-#pythonpathmunge $P4P_TOP/python${PY_LD_VER}/${EPICS_HOST_ARCH}
+# Add p4p to PYTHONPATH
+P4P_TOP=/reg/neh/home/bhill/git-wa-neh/extensions/p4p-git 
+# Note: If your branch of p4p doesn't have get_PY_LD_VER.py
+# you can just hard code your python version here.
+# Ex: PY_LD_VER=python3.7m
+PY_LD_VER=`python $P4P_TOP/get_PY_LD_VER.py`
+pythonpathmunge $P4P_TOP/python${PY_LD_VER}/${EPICS_HOST_ARCH}
+
+PY_LD_VER=`python $P4P_TOP/get_PY_LD_VER.py`
+pythonpathmunge $P4P_TOP/python${PY_LD_VER}/${EPICS_HOST_ARCH}
 
 pathmunge /reg/neh/home/bhill/git-wa-neh/extensions/loadServer-git/bin/$EPICS_HOST_ARCH
 pathmunge /reg/neh/home/bhill/git-wa-neh/extensions/stressTestClients-git/bin/$EPICS_HOST_ARCH
